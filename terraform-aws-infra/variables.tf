869a35401385ee6fde607d64e5c7689bee4c44b4
Amaan Tamboli <amantamboli671@gmail.com>
2026-03-06 19:50:28 +0530

    Update AWS region and bucket name variables

--- terraform-aws-infra/variables.tf
+++ terraform-aws-infra/variables.tf
@@ -1,12 +1,18 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  default = "us-east-1"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
 }
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
+variable "subnet_cidrs" {
+  type = list(string)
+
   default = [
     "10.0.1.0/24",
     "10.0.2.0/24",
@@ -22,5 +28,5 @@ variable "instance_type" {
 }
 
 variable "bucket_name" {
-  default = "my-terraform-demo-bucket-12345"
+  default = "terraform-research-bucket-123456"
 }
