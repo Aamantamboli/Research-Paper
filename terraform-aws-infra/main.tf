a028bafffcb0442f89301f5f35c2a7eef06c476d
Amaan Tamboli <amantamboli671@gmail.com>
2026-03-06 19:44:42 +0530

    Add VPC, EC2, and S3 modules to main.tf

---
+++ terraform-aws-infra/main.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr       = var.vpc_cidr
+  public_subnets = var.public_subnets
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  vpc_id        = module.vpc.vpc_id
+  subnet_id     = module.vpc.public_subnet_ids[0]
+  instance_type = var.instance_type
+  security_group_id = module.vpc.security_group_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = var.bucket_name
+}
