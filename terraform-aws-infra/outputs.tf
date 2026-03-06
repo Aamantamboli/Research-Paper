8bced204d2201148119d6ee6305830aa6c4f6e29
Amaan Tamboli <amantamboli671@gmail.com>
2026-03-06 19:58:49 +0530

    Update output values for VPC, subnet, EC2, and S3

--- terraform-aws-infra/outputs.tf
+++ terraform-aws-infra/outputs.tf
@@ -1,23 +1,23 @@
 output "vpc_id" {
 
-  value = aws_vpc.research_vpc.id
+  value = aws_vpc.main.id
 
 }
 
 output "subnet_ids" {
 
-  value = aws_subnet.public_subnets[*].id
+  value = aws_subnet.public[*].id
 
 }
 
 output "ec2_instance_id" {
 
-  value = aws_instance.research_instance.id
+  value = aws_instance.web.id
 
 }
 
 output "s3_bucket_name" {
 
-  value = aws_s3_bucket.research_bucket.bucket
+  value = aws_s3_bucket.bucket.bucket
 
 }
